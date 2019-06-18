e2c17a0d4b6e5334b9f2400fc2e5fbea74b8ee7f
Yauhen <q010@bk.ru>
2019-06-18 17:04:24 +0300

    added validation specs for object_class

--- server/spec/models/scp_object_spec.rb
+++ server/spec/models/scp_object_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe ScpObject, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#object_class' do
+    it "should have a valid object_class" do
+      expect { FactoryGirl.create(:scp_object, :object_class => "invelid_class") }.to raise_error(ArgumentError)
+      expect { FactoryGirl.create(:scp_object, :keter) }.not_to raise_error(ArgumentError)
+    end
+  end
 end
