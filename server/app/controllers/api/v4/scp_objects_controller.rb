9446baaadc749a4b32740fba35177167f78040a6
Yauhen <q010@bk.ru>
2019-06-19 16:36:10 +0300

    refactored json output and added create api method

--- server/app/controllers/api/v4/scp_objects_controller.rb
+++ server/app/controllers/api/v4/scp_objects_controller.rb
@@ -7,30 +7,33 @@ class Api::V4::ScpObjectsController < ApplicationController
 
   def show; end
 
-  def create; end
+  def create
+    new_object = ScpObject.create(scp_object_params)
+    render json: { success: new_object.errors.any?, object: new_object, errors: new_object.errors }
+  end
 
   def update
     object = ScpObject.find(params[:id])
     begin
       success = object.update_attributes(scp_object_params)
-      render json: { success: success, object: object }
-    rescue ArgumentError => exception
-      render json: { success: false, error: 'Error! Put in a valid attributes'  }
+      render json: { success: success, object: object, errors: object.errors }
+    rescue ActiveRecord::RecordNotFound => exception
+      render json: { success: false, errors: exception.message }
     end
   end
 
   def destroy
     begin
-      success = ScpObject.find(params[:id]).destroy
-      render json: { success: !!success, object: success }
-    rescue Exception => exception
-      render json: { success: false, error: exception.message }
+      destroying_object  = ScpObject.find(params[:id]).destroy
+      render json: { success: !!destroying_object, object: destroying_object }
+    rescue ActiveRecord::RecordNotFound => exception
+      render json: { success: false, errors: exception.message }
     end
   end
 
   private
 
   def scp_object_params
-    params.require(:scp_object).permit(%i[name object_class containment_procedures description])
+    params.require(:scp_object).permit(%i[number name object_class containment_procedures description])
   end
 end
--- server/app/models/scp_object.rb
+++ server/app/models/scp_object.rb
@@ -1,6 +1,8 @@
 class ScpObject < ApplicationRecord
   self.primary_key = :number
-  enum object_class: [:Safe, :Euclid, :Keter, :Thaumiel, :Other]
+  enum object_class_value: [:Safe, :Euclid, :Keter, :Thaumiel, :Other]
   validates :object_class,
-           inclusion: { in: object_classes.keys}
+           inclusion: { in: object_class_values.keys, message: 'this number is already taken' }
+  validates :number,
+            uniqueness: { case_sensitive: false, message: 'invalid object class' }
 end
