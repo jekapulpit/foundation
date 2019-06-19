f0137035f5bc3b3bfcde07b8cefe59d6b2501ee4
Yauhen <q010@bk.ru>
2019-06-19 13:37:46 +0300

    added update api for scp object

--- server/app/controllers/api/v4/scp_objects_controller.rb
+++ server/app/controllers/api/v4/scp_objects_controller.rb
@@ -9,8 +9,21 @@ class Api::V4::ScpObjectsController < ApplicationController
 
   def create; end
 
-  def update; end
+  def update
+    object = ScpObject.find(params[:id])
+    begin
+      success = object.update_attributes(scp_object_params)
+      render json: { success: success, object: object }
+    rescue ArgumentError => exception
+      render json: { success: false, error: exception.message }
+    end
+  end
 
   def destroy; end
 
+  private
+
+  def scp_object_params
+    params.require(:scp_object).permit(%i[name object_class containment_procedures description])
+  end
 end
