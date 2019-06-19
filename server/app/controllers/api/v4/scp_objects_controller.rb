621f7a85179ed7c99cf35d63b3ba16f5d93051e4
Yauhen <q010@bk.ru>
2019-06-19 16:09:56 +0300

    added delete method to scp_object api

--- server/app/controllers/api/v4/scp_objects_controller.rb
+++ server/app/controllers/api/v4/scp_objects_controller.rb
@@ -15,11 +15,18 @@ class Api::V4::ScpObjectsController < ApplicationController
       success = object.update_attributes(scp_object_params)
       render json: { success: success, object: object }
     rescue ArgumentError => exception
-      render json: { success: false, error: exception.message }
+      render json: { success: false, error: 'Error! Put in a valid attributes'  }
     end
   end
 
-  def destroy; end
+  def destroy
+    begin
+      success = ScpObject.find(params[:id]).destroy
+      render json: { success: !!success, object: success }
+    rescue Exception => exception
+      render json: { success: false, error: exception.message }
+    end
+  end
 
   private
 
