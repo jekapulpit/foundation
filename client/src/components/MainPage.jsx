e6346c53202d440891e02bbe64eb99691004c00a
Yauhen <q010@bk.ru>
2019-06-20 14:30:49 +0300

    fixed objects getting method

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -8,6 +8,7 @@ class MainPage extends React.Component {
 
     componentDidMount() {
         fetch('http://localhost:3001/api/v4/scp_objects/')
+            .then((response) => { return response.json() })
             .then((data) => {
                 console.log(data);
                 store.dispatch({ type: 'SET_OBJECT_LIST', objectList: data.objects })
