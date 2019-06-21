1de2edcfc86de5f4b909f9dbf12ae12c938cf7c9
Yauhen <q010@bk.ru>
2019-06-21 10:50:08 +0300

    separated action types in single file

---
+++ client/src/actionTypes.js
@@ -0,0 +1,2 @@
+export const SET_OBJECT_LIST = 'SET_OBJECT_LIST';
+export const SET_OBJECT = 'SET_OBJECT';
--- client/src/reducers/objectReduces.js
+++ client/src/reducers/objectReduces.js
@@ -1,11 +1,12 @@
 import store from "../store";
+import { SET_OBJECT, SET_OBJECT_LIST } from '../actionTypes'
 
 export default (state = { objectList: [], currentObject: {} }, action) => {
     switch (action.type) {
-        case 'SET_OBJECT_LIST':
+        case SET_OBJECT_LIST:
             let newObjectList = action.objectList;
             return {...state, objectList: newObjectList};
-        case 'SET_OBJECT':
+        case SET_OBJECT:
             let newObject = action.currentObject;
             return {...state, currentObject: newObject};
         default:
